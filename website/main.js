97b8d01cefd6272b5c11d0758582b5b16f28cfca
lemo-git <82379501+lemo-git@users.noreply.github.com>
2021-12-15 23:36:46 +0200

    Add files via upload

---
+++ website/main.js
@@ -0,0 +1,26 @@
+const breakpoint=[{
+breakpoint:1280,
+settings:{
+    SlidesToShow:4
+}
+},
+{
+    breakpoint: 1009,
+    settings:{
+        SlidesToShow:3
+    }
+},
+{
+breakpoint: 720,
+settings:{
+    SlidesToShow:2
+}
+},
+{
+  
+    breakpoint: 460,
+    settings:{
+        SlidesToShow:1
+    }
+},  
+]
